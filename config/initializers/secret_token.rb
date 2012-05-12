6ba43c821a393917aac8c5a43182f67fa800f0d7
Rich Daley <git@richd.me>
2012-05-12 10:36:57 +0100

    Commit basic bootstrap app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jelly::Application.config.secret_token = '1f14869334deb9fa721359bbba6bb4cacd0e999ea250a4da32051d332c9c3e3048bf24be01750026650b14025d3215bc05025ddc28519191f1f79ef6084ad032'
