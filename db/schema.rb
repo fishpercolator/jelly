3da73152f2e7ff098afa40d435efbc82c8778554
Rich Daley <rich@richd.me>
2012-08-04 21:14:42 +0100

    Latest schema - identical to previous due to cock-ups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120523200122) do
+ActiveRecord::Schema.define(:version => 20120804192240) do
 
   create_table "achievements", :force => true do |t|
     t.integer  "report_id"
