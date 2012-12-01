f1d32308939507253645434b5ea14d878304ca84
Rich Daley <rich@richd.me>
2012-12-01 21:19:54 +0000

    Add an apparent fix for Heroku with devise

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module Jelly
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Force app not to access DB during precompile (requirement for Heroku)
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
