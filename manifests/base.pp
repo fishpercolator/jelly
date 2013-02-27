c954b9227d7ceb70193581fd93a882de5fa4e574
Rich Daley <rich@richd.me>
2013-02-27 22:18:14 +0000

    Use magick from the apt module to always update. Hopefully this will fix the issues.

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,3 +1,4 @@
-class {"ruby-env": }
+class {"apt": always_apt_update => true; }
+class {"ruby-env": require => Class["apt"]; }
 class {"database": }
 
--- manifests/ruby-env.pp
+++ manifests/ruby-env.pp
@@ -1,15 +1,8 @@
 class ruby-env {
 
-  # fixme - not working correctly
-  exec {'apt_update':
-    command => '/usr/bin/apt-get update',
-    refreshonly => true;
-  }
-
   package {["build-essential", "ruby1.9.1-full", "libpq-dev"]:
     ensure => installed,
-    provider => apt,
-    require => Exec['apt_update'];
+    provider => apt;
   }
 
   bundler::install {"/vagrant":
