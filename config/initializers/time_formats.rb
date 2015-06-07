2d4c307ab9c7670031f9aeeff4619ce62cc31324
Rich Daley <rich@owl.me.uk>
2015-06-07 16:31:09 +0100

    Fix the 'show' action and the signup form

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,3 @@
-Date::DATE_FORMATS[:default] = "%a %d %b %Y"
-Time::DATE_FORMATS[:default] = "%a %d %b %Y %H:%M"
+Date::DATE_FORMATS[:default] = "%a %e %b %Y"
+Time::DATE_FORMATS[:default] = "%a %e %b %Y %H:%M"
 
