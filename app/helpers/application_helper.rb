877f46eb2401d8ecfaf271f64cfeb037cc3d60d6
Rich Daley <rich@owl.me.uk>
2015-06-07 13:12:31 +0100

    Glyphs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,5 +5,10 @@ module ApplicationHelper
     options[:gravatar] = {:size => size, :default => :monsterid}
     gravatar_image_tag user.email, options
   end
+
+  def glyph(icon, description=nil)
+    description ||= icon
+    raw %{<span class="glyphicon glyphicon-#{icon}" aria-hidden="true" aria-label="#{description}"></span>}
+  end
   
 end
