997f51636003166a38d9242b4234378960e37a21
Rich Daley <rich@owl.me.uk>
2015-06-12 23:05:50 +0100

    Add markdown to excited and help

--- Gemfile
+++ Gemfile
@@ -37,7 +37,8 @@ gem 'gravatar_image_tag', '~> 1.2.0'
 #gem 'jquery-ui-sass-rails', '~> 4.0.3.0'
 gem 'bootstrap-datepicker-rails', '~> 1.4.0'
 gem 'rails-reveal-js', github: 'fishpercolator/rails-reveal-js'
-gem 'animate.css-rails'
+gem 'animate.css-rails', '~> 3.2.0'
+gem 'redcarpet', '~> 3.3.1'
 
 # Gem used by Heroku to avoid plugin injection
 group :production do
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,12 @@ module ApplicationHelper
     description ||= icon
     raw %{<span class="glyphicon glyphicon-#{icon}" aria-hidden="true" aria-label="#{description}"></span>}
   end
+
+  # Use Redcarpet to add a markdown helper
+  def markdown(text)
+    return if !text
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(escape_html: true), autolink: true, space_after_headers: true, fenced_code_blocks: true)
+    @markdown.render(text).html_safe
+  end
   
 end
