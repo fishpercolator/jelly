9001ee2897be7ab8ab73a1a2e0a5033985673c49
Rich Daley <rich@owl.me.uk>
2015-06-14 13:27:35 +0100

    Oops - make sure secret key is not hardcoded

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Jelly::Application.config.secret_token = '1f14869334deb9fa721359bbba6bb4cacd0e999ea250a4da32051d332c9c3e3048bf24be01750026650b14025d3215bc05025ddc28519191f1f79ef6084ad032'
+Jelly::Application.config.secret_token = ENV['SECRET_KEY_BASE']
+ 
